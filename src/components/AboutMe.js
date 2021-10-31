de36930c8f0c5c9a0d5ee8d43ed8330e0f8cd297
juadgarciagom <juadgarciagom@outlook.com>
2021-10-30 20:44:47 -0500

    test

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,7 +7,7 @@ export const AboutMe = () => {
             <h3>Juan David Garcia</h3>
             <p>Soy una persona proactiva, con la mejor disposición para aprender de manera rápida y eficaz. Siempre listo para trabajar y colaborar
                 como parte de un equipo. Motivado para contribuir con mi conocimiento a la solución de los retos que se me impongan
-                y a adquirir nuevos conocimientos a partir de estas experiencias. </p>
+                y a adquirir nuevos conocimientos a partir de estas. </p>
             <ResumeButton />
         </div>
     )
